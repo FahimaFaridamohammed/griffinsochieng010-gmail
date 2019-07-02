3c5eaa971976a66ece5aa82d6a39e4ea0f2d81c4
AC05 <oderocharlesouma@gmail.com>
2019-07-02 17:06:38 +0300

    the second update

--- app/src/main/java/www/grif/calculator/MainActivity.java
+++ app/src/main/java/www/grif/calculator/MainActivity.java
@@ -2,8 +2,19 @@ package www.grif.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edt1;
+    private Button  button1;
+    private  Button  button2;
+    private  Button  button3;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
